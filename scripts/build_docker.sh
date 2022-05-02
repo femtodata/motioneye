2358e07c9653fd9c7cfd0c6721ca715308647035
Alexander Ou <alexou@femtodata.com>
2022-05-02 14:36:48 -0700

    docker build stuff

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $(dirname $0)/..
-docker build --build-arg VCS_REF=$(git rev-parse HEAD) --build-arg BUILD_DATE=$(date +"%Y-%m-%dT%H:%M:%SZ") -t femtodata/motioneye:dev-amd64 -f extra/Dockerfile .
+docker build --build-arg VCS_REF=$(git rev-parse HEAD) --build-arg BUILD_DATE=$(date +"%Y-%m-%dT%H:%M:%SZ") -t femtodata/motioneye:dev-amd64 -f docker/Dockerfile .
